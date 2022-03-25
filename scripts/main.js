366e1dd874675001e7e026506a66340040a3dd0a
LuukNoverraz <luuk.noverraz@coderclass.nl>
2022-03-25 19:43:21 +0100

    Add dynamic wave height with js

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+const body = document.body;
+const html = document.documentElement;
+const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight, html.scrollHeight, html.offsetHeight);
+
+document.getElementById("wave-container").style.height = height + "px";
