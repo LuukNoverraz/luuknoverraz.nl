1da4701aa3f4b4e5eb64c0bc442326c26b4a8529
LuukNoverraz <luuk.noverraz@coderclass.nl>
2022-03-29 16:21:24 +0200

    Animate header on scroll

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,15 @@ const height = Math.max(body.scrollHeight, body.offsetHeight, html.clientHeight,
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var navbarHeight = $('.header').outerHeight();
+
+// Change wave container height based on screen size
 document.getElementById("wave-container").style.height = height + "px";
 
+// Open collapsible content and close/open on click
 for (i = 0; i < coll.length; i++) {
     coll[i].classList.toggle("active");
     coll[i].nextElementSibling.style.maxHeight = coll[i].nextElementSibling.scrollHeight + "px";
@@ -19,4 +26,38 @@ for (i = 0; i < coll.length; i++) {
         content.style.maxHeight = content.scrollHeight + "px";
     } 
     });
+}
+
+$(window).scroll(function(event){
+    didScroll = true;
+});
+
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
+
+function hasScrolled() {
+    var st = $(this).scrollTop();
+    
+    // Make sure they scroll more than delta
+    if(Math.abs(lastScrollTop - st) <= delta) {
+        return;
+    }
+
+    // If they scrolled down and are past the navbar, add class .nav-up.
+    // This is necessary so you never see what is "behind" the navbar.
+    if (st > lastScrollTop && st > navbarHeight){
+        // Scroll Down
+        $('.header').removeClass('nav-down').addClass('nav-up');
+    } else {
+        // Scroll Up
+        if(st + $(window).height() < $(document).height()) {
+            $('.header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    
+    lastScrollTop = st;
 }
