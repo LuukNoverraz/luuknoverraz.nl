ab8ad592556189f8d0c3f6c9ca51cbf5d50b45f4
Magmaroni <luuknoverraz@gmail.com>
2023-11-09 17:22:26 +0100

    Added English translation

--- scripts.js
+++ scripts.js
@@ -1,3 +1,155 @@
+const translations = {
+    en: {
+        headerAbout: "About me",
+        subtitle: "With a passion for creativity, I design and develop games, websites and video content.",
+        project1Slogan: "I build digital worlds and stories",
+        project2Slogan: "I make user-friendly web experiences",
+        project3Slogan: "I share knowledge and humor through videos",
+        projectBack: "Back",
+        project1Duration: "2020 (4 weeks)",
+        project1Text1: "Barbasim is a 'creature simulation' that I created in 2020 with the Unity engine. A random island is generated where little \"Barba\" creatures interact with each other. Each Barba has unique statistics and its own name. You can view the island from different angles and follow Barba's.",
+        project1Text2: "In my design process I iterated through many different ideas. Originally I planned to use an isometric perspective. The creatures would be blocky and move in fixed steps. However, I wanted to add more dynamics, so I started thinking further. So I sketched a few 2D concepts.",
+        project1Text3: "However, I decided to do it in 3D next, because I could easily produce many creatures by making some adjustments to the mesh, which would be more difficult with a sprite. The first design I made in 3D was a block with legs. Only I concluded that animals consist of soft shapes and are not sharp and rectangular. So I redesigned the creatures later in the process.",
+        project1Text4: "After this I focused on making the world. Originally I was going for an infinite world, but I realized that in an infinite world you would become less attached to the creatures. That's why I decided to create an island for the simulation, so that you could recognize the specific animals on the island.",
+        project1Text5: "I then wrote a generator to create the creatures, which I decided to call Barba's, each with a different color, size, and name. I also wrote a name generator myself for that. I made sure to have a few hundred Barba's born at the start of the game, each with different stats. In addition to color, height and name, I also added strength, friendliness and speed. Ultimately, I made it so that the Barba's could also reproduce, having children with their parents' stats and dying by drowning or getting into a fight.",
+        project1Text6: "In the end, I created a game that I am still proud of three years later. Barbasim can be downloaded from Itch.io.",
+        project2Duration: "2023 (4 weeks)",
+        project2Text1: "I designed and created a one-page website for company De Streng. This company had an outdated site that was in need of a refresh. Based on the previous site, I first redesigned the company's brand identity into a more modern and simplistic image. In consultation with the client, I decided to create a one-page website, mainly because of space savings and a more pleasant user experience.",
+        project2Text2: "First I designed the site for both desktop and mobile formats. After some discussions with the client, I went through a few iterations. After this I started building the website in HTML/CSS.",
+        project2Text3: "Although the site is basically static, there are some dynamic elements on the page for which I deployed JavaScript. In addition, I have ensured that the site's code is neat and semantically correct. After a few weeks the site was ready and available in both Dutch and English. The site is responsive and therefore works on desktop, tablet and mobile.",
+        project2Text4: "Ultimately, I am very satisfied with how the site turned out and fortunately so was the client. It was very educational to be able to carry out the entire process, from design to development, at the service of this customer.",
+        project3Duration: "2023 - present",
+        project3Text1: "For over 5 years I have been making videos with Adobe Premiere Pro. Recently, I started making videos on my YouTube channel called Magmaroni. Here, I aim to share my game development journey and process.",
+        aboutTitle: "About me",
+        aboutText1: "Currently I'm a ",
+        aboutText2: " year student Communication & Multimedia Design at the Amsterdam University of Applied Sciences. Here I learn to find creative solutions to unique design challenges with an eye for both users and clients. I pay attention to the smallest details and am always eager to learn. I consider my understanding of both design and technology to be my strength and hope to use this overlap to contribute to a better world.",
+        aboutText3: "If you are interested in my work or would like to collaborate, please don't hesitate to contact me at",
+        experience: "Experience",
+        workExperience: "Work experience",
+        experienceTitle1: "Web Developer",
+        experienceDuration1: "February 2022 - Present",
+        experienceDescription1: "Managing an online store in Shopify, including making adjustments in Liquid and CSS. Furthermore, I designed and developed a responsive one-page website.",
+        experienceTitle2: "Web Developer",
+        experienceDuration2: "August 2020 - September 2021",
+        experienceDescription2: "Worked on improving the design of Coolfinity's website and developed it with Wix.",
+        experienceTitle3: "Graphic Design Intern",
+        experienceTitle4: "Communication and Multimedia Design (Bachelor)",
+        experienceTitle4School: "/ Amsterdam University of Applied Sciences",
+        experienceDuration4: "September 2022 - Present",
+        experienceDescription4: "Obtained a propaedeutic diploma cum laude in 2023.",
+        education: "Education",
+        skills: "Skills",
+        languages: "Languages",
+        languageDutch: "Dutch / Native language",
+        languageEnglish: "English / Full professional fluency"
+        // More translations
+    },
+    nl: {
+        headerAbout: "Over mij",
+        subtitle: "Met een passie voor creativiteit ontwerp en ontwikkel ik games, websites en videocontent.",
+        project1Slogan: "Ik bouw digitale werelden en verhalen",
+        project2Slogan: "Ik maak user-friendly web-ervaringen",
+        project3Slogan: "Ik deel kennis en humor via video’s",
+        projectBack: "Terug",
+        project1Duration: "2020 (4 weken)",
+        project1Text1: "Barbasim is een 'creature simulation' die ik in 2020 met de Unity engine heb gemaakt. Er wordt een willekeurig eiland gegenereerd waar kleine \"Barba\" wezentjes met elkaar interacteren. Elke Barba heeft unieke statistieken en een eigen naam. Je kunt het eiland vanuit verschillende hoeken bekijken en Barba's volgen.",
+        project1Text2: "In mijn ontwerpproces ben ik door veel verschillende ideeën geïtereerd. Oorspronkelijk was ik van plan een isometrisch perspectief toe te passen. Daarbij zouden de beestjes blokkig zijn en in vaste stappen bewegen. Echter wilde ik meer dynamiek toevoegen, dus ging ik verder nadenken. Zo heb ik een paar 2D-concepten geschetst.",
+        project1Text3: "Echter besloot ik het hierna in 3D uit te voeren, omdat ik dan makkelijk veel wezens zou kunnen produceren door enkele aanpassingen aan de mesh te maken, wat bij een sprite lastiger zou zijn. Het eerste ontwerp dat ik in 3D heb gemaakt was een blokje met benen. Alleen concludeerde ik dat dieren bestaan uit zachte vormen en niet scherp en rechthoekig zijn. Daarom heb ik de wezens later in het proces herontworpen.",
+        project1Text4: "Hierna heb ik mij gefocust op het maken van de wereld. Oorspronkelijk ging ik voor een oneindige wereld, maar ik realiseerde dat in een oneindige wereld je minder gehecht zou raken aan de wezens. Daarom besloot ik toch te gaan voor het maken van een eiland bij de simulatie, zodat je de specifieke dieren op het eiland zou kunnen herkennen.",
+        project1Text5: "Ik schreef vervolgens een generator om om de wezens, die ik Barba's besloot te noemen, te maken met elk een andere kleur, lengte en naam. Daarvoor heb ik ook zelf een naam generator geschreven. Ik zorgde ervoor dat er aan het begin van het spel een paar honderd Barba’s werden geboren, elk met andere statistieken. Naast kleur, lengte en naam voegde ik ook sterkte, vriendelijkheid en snelheid toe. Uiteindelijk zorgde ik ervoor dat de Barba’s ook voort konden planten en zo kinderen konden krijgen met de statistieken van hun ouders en dood konden gaan door te verdrinken of door in een gevecht te raken.",
+        project1Text6: "Zo heb ik uiteindelijk een spel gemaakt waar ik nog steeds drie jaar later trots op ben. Barbasim is te downloaden op Itch.io.",
+        project2Duration: "2023 (4 weken)",
+        project2Text1: "Ik heb voor bedrijf De Streng een one-page website ontworpen en gemaakt. Dit bedrijf had een verouderde site die toe was aan een verfrissing. Op basis van de voorgaande site heb ik eerst de merkidentiteit van het bedrijf herontworpen naar een moderner en simplistischer beeld. In overleg met de opdrachtgever heb ik besloten een one-page website te maken, vooral vanwege ruimtebesparing en een meer prettige user experience.",
+        project2Text2: "Eerst heb ik de site ontworpen, voor zowel desktop als mobiel formaat. Na enkele besprekingen met de opdrachtgever ben ik enkele iteraties doorgegaan. Hierna ben ik begonnen met het bouwen van de website in HTML/CSS.",
+        project2Text3: "Hoewel de site in principe statisch is, zijn er enkele dynamische elementen op de pagina waarvoor ik JavaScript heb ingezet. Daarnaast heb ik erop gelet dat de code van de site netjes en semantisch correct is. Na enkele weken was de site gereed en in zowel Nederlands als Engels beschikbaar. De site is responsive en werkt dus op desktop, tablet en mobiel.",
+        project2Text4: "Uiteindelijk ben ik erg tevreden met hoe de site is geworden en dat was de opdrachtgever gelukkig ook. Het was erg leerzaam om het volledige proces, van ontwerp tot ontwikkeling, in dienst van deze klant uit te kunnen voeren.",
+        project3Duration: "2023 - heden",
+        project3Text1: "Al ruim 5 jaar maak ik video's met Adobe Premiere Pro. Onlangs ben ik begonnen met het maken van video's op mijn YouTube-kanaal genaamd Magmaroni. Hier wil ik mijn game-ontwikkelingsreis en -proces delen.",
+        aboutTitle: "Over mij",
+        aboutText1: "Momenteel ben ik ",
+        aboutText2: " jaars student Communication & Multimedia Design aan de Hogeschool van Amsterdam. Hier leer ik met een oog voor zowel gebruikers als opdrachtgevers creatieve oplossingen te vinden tot unieke design challenges. Ik let op de kleinste details en ben altijd leergierig. Ik beschouw mijn begrip van zowel ontwerp als technologie als mijn kracht en hoop deze overlap in te kunnen zetten om bij te kunnen dragen aan een betere wereld.",
+        aboutText3: "Als je geïnteresseerd bent in mijn werk of wilt samenwerken, aarzel dan niet om contact met me op te nemen via",
+        experience: "Ervaring",
+        workExperience: "Werkervaring",
+        experienceTitle1: "Webontwikkelaar",
+        experienceDuration1: "Februari 2022 - heden",
+        experienceDescription1: "Bezig met het beheren van een webshop in Shopify, inclusief het maken van aanpassingen in Liquid en CSS. Bovendien een responsive one-page website ontworpen en ontwikkeld.",
+        experienceTitle2: "Webontwikkelaar",
+        experienceDuration2: "Augustus 2020 - september 2021",
+        experienceDescription2: "Gewerkt aan het verbeteren van het ontwerp van de website van Coolfinity en ontwikkeld met Wix.",
+        experienceTitle3: "Stagiair Grafisch Ontwerp",
+        experienceTitle4: "Communication and Multimedia Design (Bachelor)",
+        experienceTitle4School: "/ Hogeschool van Amsterdam",
+        experienceDuration4: "September 2022 - heden",
+        experienceDescription4: "Propedeuse in 2023 cum laude behaald.",
+        education: "Opleidingen",
+        skills: "Vaardigheden",
+        languages: "Talen",
+        languageDutch: "Nederlands / Moedertaal",
+        languageEnglish: "Engels / Professionele vaardigheid"
+        // More translations
+    },
+    // More languages
+};
+
+const nl = document.getElementById('nl');
+const en = document.getElementById('en');
+
+// Get the current date
+const currentDate = new Date();
+
+// Determine the year value based on the current date and condition
+let yearValue = 2; // Default value
+
+if (currentDate > new Date('2024-09-30')) {
+    yearValue = 3;
+}
+
+if (currentDate > new Date('2025-09-30')) {
+    yearValue = 4;
+}
+
+function translatePage(language) {
+    if (language == "nl") {
+        nl.style.color = 'var(--red)';
+        en.style.color = 'var(--black)';
+    }
+    if (language == "en") {
+        nl.style.color = 'var(--black)';
+        en.style.color = 'var(--red)';
+    }
+
+    let suffix;
+
+    if (language === "nl") {
+        suffix = "e";
+    } else if (language === "en") {
+        if (yearValue === 1) {
+            suffix = "st";
+        } else if (yearValue === 2) {
+            suffix = "nd";
+        } else if (yearValue === 3) {
+            suffix = "rd";
+        } else {
+            suffix = "th";
+        }
+    }
+
+    // Update the content of the span with the calculated year value and suffix
+    document.getElementById('studyYear').textContent = `${yearValue}${suffix}`;
+    
+    const elements = document.querySelectorAll('[data-translate]');
+    elements.forEach(element => {
+        const key = element.getAttribute('data-translate');
+        if (translations[language] && translations[language][key]) {
+            element.textContent = translations[language][key];
+        }
+    });
+}
+
+// Example: Translate the page to Dutch
+translatePage('nl');
+
 // Get a reference to the <a> element
 const index = document.getElementById('header-index');
 const about = document.getElementById('header-about');
@@ -40,23 +192,6 @@ const copyrightElement = document.querySelector('.copyright');
 
 // Update the text content with the current year
 copyrightElement.textContent = copyrightElement.textContent.replace('20XX', currentYear);
-
-// Get the current date
-const currentDate = new Date();
-
-// Determine the year value based on the current date and condition
-let yearValue = 2; // Default value
-
-if (currentDate > new Date('2024-09-30')) {
-    yearValue = 3;
-}
-
-if (currentDate > new Date('2025-09-30')) {
-    yearValue = 4;
-}
-
-// Update the content of the span with the calculated year value
-document.getElementById('studyYear').textContent = `${yearValue}e`;
 
 const projects = document.querySelectorAll('.project');
 const additionalContent = document.querySelector('.additional-content');
@@ -121,4 +256,18 @@ closeButton.addEventListener('click', () => {
     gameProject.style.display = 'none';
     webProject.style.display = 'none';
     videoProject.style.display = 'none';
+});
+
+// Get all elements with the class "language-item"
+const languageItems = document.querySelectorAll('.language-item');
+
+// Loop through each element and apply the italic style
+languageItems.forEach(element => {
+    const text = element.textContent;
+    const parts = text.split('/');
+    
+    if (parts.length === 2) {
+        const newText = parts[0].trim() + ' / <i>' + parts[1].trim() + '</i>';
+        element.innerHTML = newText;
+    }
 });
